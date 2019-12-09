6c2e2915cd92cba3cbcc1e6e29f03db73b46db6e
dplicn <3235647@qq.com>
2019-12-09 20:50:30 +0800

    file dialog

---
+++ userfile.py
@@ -0,0 +1,25 @@
+#引用文件选择对话框
+from tkinter.filedialog import askopenfilename
+from tkinter.filedialog import asksaveasfilename
+from tkinter.filedialog import askopenfilename
+def main():
+    print("this program creates a file of username from a")
+    print("file of names")
+    #get the file names
+    infileName = askopenfilename()
+    outfileName = asksaveasfilename()
+
+    #open the files
+    infile = open(infileName,'r')
+    outfile = open(outfileName,'w')
+    #
+    for line in infile:
+        first,last = line.split()
+        userName = first[0].upper() + last[0:7].lower()
+        print(userName,file = outfile)
+
+    #close file
+    infile.close()
+    outfile.close()
+
+main()
