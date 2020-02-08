7e136fc834d5ae0b10f5818fb769ad31e16aea7f
dplicn <3235647@qq.com>
2020-02-08 14:26:06 +0800

    .

---
+++ heimaPython/18列表推导式.py
@@ -0,0 +1,39 @@
+'''
+列表推导式
+1,由for循环创建的列表
+2,由列表推导式创建的列表
+3,带if的列表推导式
+4,多for推导式 实现[(1,0),(1,1),(1,2),(2,0),(,2,1),(2,2)]
+'''
+# 1,由for循环创建的列表
+# l1 = []
+# for i in range(10):
+#     l1.append(i)
+# print(l1)
+
+# 2,由列表推导式创建的列表
+# l1 = [i for i in range(10)]
+# print(l1)
+# 3,带if的列表推导式
+l1 = []
+# for i in range(10):
+#     if(i%2==0):
+#         l1.append(i)
+# print(l1)
+
+# l1 = [i for i in range(10) if i%2==0]
+# print(l1)
+
+# 4,多for推导式 实现[(1,0),(1,1),(1,2),(2,0),(,2,1),(2,2)]
+# l1 = []
+# for i in range(1,3):
+#     for j in range(3):
+#         l1.append((i,j))
+    
+# print(l1)
+
+
+l1 = [(i,j) for i in range(1,3) for j in range(3)]
+print(l1)7
+
+d = {}
---
+++ heimaPython/19类和self.py
@@ -0,0 +1,15 @@
+
+
+class xiYiJi():
+    '''洗衣机类'''
+    def xiYiFu(self):
+        '''洗衣服'''
+        print("洗衣服")
+    def tuoShui(self):
+        '''脱水'''
+        print('脱水')
+
+
+haier = xiYiJi()
+
+haier.tuoShui()
