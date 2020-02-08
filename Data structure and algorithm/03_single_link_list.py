ed113bb46a90087bff5196c238b8cd3b23048e6f
dplicn <3235647@qq.com>
2020-02-08 14:39:29 +0800

    0208

---
+++ Data structure and algorithm/03_single_link_list.py
@@ -0,0 +1,44 @@
+# # 单向链表
+
+# class Node(elem):
+#     '''结点类'''
+#     Elem = elem
+#     Next = None
+
+# class SingleLinkList(node):
+#     '''单项链表类'''
+#     def __init__(selft):
+#         pass
+
+#     def is_empty(self):
+#         pass
+
+#     def lenth(self):
+#         '''返回长度'''
+#         pass
+
+#     def travel(self):
+#         '''遍历打印'''
+#         pass
+
+#     def add(self):
+#         '''头插法'''
+#         pass
+#     def append(self):
+#         '''尾插法'''
+#         pass
+#     def insert(self):
+#         '''按位置插入'''
+#         pass
+#     def remove(self):
+#         '''删除指定位置元素'''
+#         pass 
+#     def search(self):
+#         '''搜索元素'''
+#         pass
+
+        
+
+# def main():
+#     sll = SingleLinkList()
+#     node1 = Node(200)
